b6a87c73d1887372a03f91fc44890e68c6a708eb
adeliovdlk <adelioa.c@gmai.com>
2020-12-14 11:01:17 -0300

    Banco de dados H2 e criacao automaticao da base de dados

--- src/main/java/com/cursospringboot/spring_boot/domain/Categoria.java
+++ src/main/java/com/cursospringboot/spring_boot/domain/Categoria.java
@@ -1,4 +1,54 @@
 package com.cursospringboot.spring_boot.domain;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+@Entity
+public class Categoria implements Serializable {
+    public static final long serialVersionUID=1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Categoria() {
+    }
+
+    public Categoria(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Categoria categoria = (Categoria) o;
+
+        return id != null ? id.equals(categoria.id) : categoria.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
